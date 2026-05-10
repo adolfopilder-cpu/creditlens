77e3a517ef370caa66b102c09a6ce8e6c85c220c
adolfopilder-cpu <adolfopilder@hotmail.com>
2026-05-10 06:56:09 -0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -19,7 +19,7 @@ from reportlab.lib.pagesizes import A4
 from reportlab.lib.styles import ParagraphStyle
 from reportlab.lib.units import cm
 from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Table, TableStyle, HRFlowable
-
+from reportlab.lib.enums import TA_CENTER, TA_LEFT, TA_RIGHT
 try:
     import pdfplumber
     HAS_PDFPLUMBER = True
