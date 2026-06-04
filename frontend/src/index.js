bf639a750a2c2fa87d148df3fdcf0f26694259a0
adolfopilder-cpu <adolfopilder@hotmail.com>
2026-06-03 23:56:33 -0300

    Remove React entry point and add HTML template
    Replaced React app entry point with HTML template.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+<!DOCTYPE html>
+<html lang="pt-BR">
+  <head>
+    <meta charset="utf-8" />
+    <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="theme-color" content="#1E3A5F" />
+    <meta
+      name="description"
+      content="P.I.L.D.E.R CreditLens"
+    />
+    <title>P.I.L.D.E.R CreditLens</title>
+  </head>
+  <body>
+    <noscript>Você precisa habilitar JavaScript para rodar este app.</noscript>
+    <div id="root"></div>
+  </body>
+</html>
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<React.StrictMode><App /></React.StrictMode>);
